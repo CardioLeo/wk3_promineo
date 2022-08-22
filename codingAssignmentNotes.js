01c1434fa9964acaa7982fe3ad3a9cc5e075829d
echocslsk <CardioLeo@gmail.com>
2022-08-21 22:25:32 -0600

    updates to second problem

--- codingAssignmentNotes.js
+++ codingAssignmentNotes.js
@@ -26,3 +26,60 @@ for (let i = 0; i < ages.length; i++) {
 console.log(Math.round(sum/ages.length));
 
 }
+
+challengeOne();
+
+---
+
+1.a-c.
+
+function challengeOne() {
+const ages = [3, 9, 23, 64, 2, 8, 28, 93];
+//step 1.a.:
+console.log(ages[ages.length -1] - ages[0]);
+//age 1.b.:
+ages.push(67);
+console.log(ages[ages.length -1] - ages [0]);
+//age 1.c.:
+let sum = 0
+for (let i = 0; i < ages.length; i++) {
+        sum=+ages[i];
+}
+console.log(Math.round(sum/ages.length));
+}
+ challengeOne();
+
+
+---
+
+---
+
+2.
+
+
+
+
+
+//	Attempt wrote out in class:
+
+
+function challengeTwo() {
+
+const names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
+let sum = 0;
+
+for (let i = 0; i < names.length; i++) {
+        sum += names[i].length;
+}
+
+console.log(sum / names.length);
+
+let next = "";
+for (let i = 0; i < names.length; i++) {
+        next += names [i] + " ";
+}
+
+console.log(next.trim()); //trim takes away leading or ending spaces, so that's nice
+
+}
+
