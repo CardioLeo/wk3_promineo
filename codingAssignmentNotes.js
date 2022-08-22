ae469cca606ff177d69d3e1974c7f573df8aa4fa
echocslsk <CardioLeo@gmail.com>
2022-08-21 22:58:57 -0600

    takes doc up to 5th problem for coding assignment

--- codingAssignmentNotes.js
+++ codingAssignmentNotes.js
@@ -3,35 +3,16 @@
 // Date Modified: 08/21/22
 // Description: A place to keep notes as I prep to complete the coding assignment steps.
 
-1.
+
+//---
+//***
+//---
+
+console.log("Start process for challengeOne...\n...\n...\n... \n \n \n \n\n");
 
 //ages.shift();
-
-function challengeOne() {
-
-const ages = [3, 9, 23, 64, 2, 8, 28, 93];
-
-console.log(ages[ages.length -1] - ages[0]);
-
-ages.push(45);
-
-console.log(ages[ages.length -1] - ages [0]);
-
-let sum = 0
-
-for (let i = 0; i < ages.length; i++) {
-	sum=+ages[i];
-}
-
-console.log(Math.round(sum/ages.length));
-
-}
-
-challengeOne();
-
----
-
-1.a-c.
+//1.a-c.
+//
 
 function challengeOne() {
 const ages = [3, 9, 23, 64, 2, 8, 28, 93];
@@ -47,39 +28,110 @@ for (let i = 0; i < ages.length; i++) {
 }
 console.log(Math.round(sum/ages.length));
 }
- challengeOne();
+
+challengeOne();
 
 
----
+//---
+//***
+//---
 
----
+console.log("Start process for challengeTwo...\n...\n...\n... \n \n \n \n\n");
 
-2.
-
-
-
-
-
-//	Attempt wrote out in class:
+//2.
+//
 
 
 function challengeTwo() {
-
 const names = ["Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];
 let sum = 0;
-
 for (let i = 0; i < names.length; i++) {
         sum += names[i].length;
 }
-
 console.log(sum / names.length);
-
 let next = "";
 for (let i = 0; i < names.length; i++) {
         next += names [i] + " ";
 }
-
 console.log(next.trim()); //trim takes away leading or ending spaces, so that's nice
-
 }
 
+challengeTwo();
+
+
+//---
+//***
+//---
+
+console.log("Start process for challengeThree...\n...\n...\n... \n \n \n \n\n");
+
+//3.
+//
+
+
+const shades_of_red = ["scarlet", "maroon", "purple", "reddish-purple", "reddish-pink", "pinkish", "red"];
+const red=shades_of_red[shades_of_red.length - 1];
+function challengeThree() {
+console.log(red);
+return 0;
+}
+
+challengeThree();
+
+
+//---
+//***
+//---
+
+console.log("Start process for challengeFour...\n...\n...\n... \n \n \n \n\n");
+
+//4.
+//
+
+
+function challengeFour() {
+console.log(shades_of_red[0]);
+return 0;
+}
+
+challengeFour();
+
+
+//---
+//***
+//---
+
+console.log("Start process for challengeFive...\n...\n...\n... \n \n \n \n\n");
+
+//5.
+//
+
+
+function challengeFive() {
+const namesArray = ["Kellog", "Samantha", "Katlyn"];
+	for (i = 0; i < namesArray.length -1; i++) {
+		let length = namesArray[i.length];
+		let namesLength = []
+		return namesLength;	
+	}
+}
+
+challengeFive();
+
+
+//---
+//***
+//---
+
+console.log("Start process for challengeSix...\n...\n...\n... \n \n \n \n\n");
+
+//6.
+//
+
+
+function challengeSix() {
+
+return 0;
+}
+
+challengeSix();
